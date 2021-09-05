6b6c3a6ba1bc16e63e146ed5d98c21e73c756fb8
salma <kcb@Salma-MacBook-Pro.local>
2021-09-05 11:28:15 +0700

    Initial commit

---
+++ app/src/main/java/com/idn/belajarintent/IntentDenganData.kt
@@ -0,0 +1,28 @@
+package com.idn.belajarintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.ResultReceiver
+import android.widget.TextView
+
+class IntentDenganData : AppCompatActivity() {
+
+    companion object{
+        const val EXTRA_TEXT ="extra_text"
+    }
+
+    private  lateinit var tvReceiver : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_dengan_data)
+
+        tvReceiver = findViewById(R.id.tv_penerima_data)
+
+        val textReceived = intent.getStringExtra(EXTRA_TEXT)
+
+        val text ="Text yang diterima : $textReceived "
+
+        tvReceiver.text = text
+    }
+}
